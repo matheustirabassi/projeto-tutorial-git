4e4269f90cc51ff084e35da5b7e204956cc6402f
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-08-31 19:22:10 -0300

    fazendo merge

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		System.out.println("Testando o Java :)");
+		// comentário conflitante hehe
 		System.out.println("Olá mundo!");
 
 	}
